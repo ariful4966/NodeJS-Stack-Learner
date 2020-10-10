d03cf836cf1d858e519dbfd16f00929f6406ab83
ariful4966 <ariful4966@gmail.com>
2020-10-10 23:06:27 +0530

    model created

---
+++ 12._Project-Stacture/project/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.json({
+        message: 'Hello World'
+    })
+})
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on PORT ${PORT}`)
+});
+
+//12.4 complete
---
+++ 12._Project-Stacture/project/models/Profile.js
@@ -0,0 +1,45 @@
+//User, Title, bio , profilepics, links:{fb, twiter}, post, bookmarks
+
+const {Schema, model}=   require('mongoose');
+const Post = require('./Post');
+const User = require('./User');
+
+
+const profileSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: User,
+        required: true
+    },
+    
+        title:{
+            type: String,
+            trim: true,
+            maxlength: 100
+        },
+        bio:{
+            type: String,
+            trim: true,
+            maxlength: 500
+        },
+        profilePic: String,
+        links:{
+            website: String,
+            facebook: String,
+            twitter: String,
+            github: String
+        },
+    posts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: Post
+        }
+    ],
+    bookmark:{
+        type: Schema.Types.ObjectId,
+        ref: Post
+    }
+},{timestamps: true})
+
+const Profile = model('Profile', profileSchema);
+module.exports = Profile;
