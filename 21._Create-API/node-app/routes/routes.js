c617b66733e2b71c0dd34d86ff78555f6441697b
ariful4966 <ariful4966@gmail.com>
2020-11-07 21:08:24 +0600

    Dashboard complete

---
+++ 21._Create-API/node-app/routes/routes.js
@@ -0,0 +1,54 @@
+
+const authRoute = require('./authRoute');
+const dashboardRoute = require('./dashboardRoute');
+const playgroundRoute = require('../playground/play');
+const uploadRoute = require('./uploadRoutes');
+const postRoute = require('./postRoutes');
+
+const apiRoutes = require('../api/routes/apiRoutes');
+
+const routes = [
+    {
+        path: '/auth',
+        handler: authRoute
+    },
+    {
+        path: '/dashboard',
+        handler: dashboardRoute
+    },
+    {
+        path: '/uploads',
+        handler: uploadRoute
+    },
+    {
+        path: '/posts',
+        handler: postRoute
+    },
+    {
+        path: '/playground',
+        handler: playgroundRoute
+    },
+    {
+        path: '/api',
+        handler: apiRoutes 
+    },
+    {
+        path: '/',
+        handler: (req, res) => {
+            res.json({
+                message: 'Hello World'
+            })
+        }
+    }
+]
+
+module.exports = app => {
+    routes.forEach(r => {
+        if (r.path === '/') {
+            app.get(r.path, r.handler)
+        } else {
+            app.use(r.path, r.handler)
+        }
+
+    })
+}
